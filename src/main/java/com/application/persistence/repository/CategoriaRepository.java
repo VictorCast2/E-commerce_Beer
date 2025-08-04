f9688f0d13fccb1e47c69067b1dd504a883d78c3
J0SE <joseandrestorres0508@gmail.com>
2025-08-04 12:07:19 -0500

    refactor: corregir configuración de la base de datos

--- src/main/java/com/application/persistence/repository/CategoriaRepository.java
+++ src/main/java/com/application/persistence/repository/CategoriaRepository.java
@@ -1,18 +1,10 @@
 package com.application.persistence.repository;
 
 import com.application.persistence.entity.categoria.Categoria;
-import com.application.persistence.entity.producto.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
