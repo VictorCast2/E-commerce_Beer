de370410e12a56e899f9feb27d0020c01141e860
keyner reyes <reyeskeyner110@gmail.com>
2025-08-09 22:01:27 -0500

    Add product, login, and registration pages with assets
    Added new CSS files for login, registration, and product pages. Introduced new images for About Us, banners, and product sections. Updated style.css with layout and section improvements. Added new HTML templates for login, registration, and product listing, and updated Index.html. Added JavaScript for product functionality.

--- src/main/resources/static/Js/main.js
+++ src/main/resources/static/Js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     // Efecto glassmorphism solo al hacer scroll
     const header = document.querySelector('.header');
 
@@ -34,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         currentSlide = index;
     }
+
 
     // Siguiente slide
     function nextSlide() {
@@ -86,56 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
     startAutoplay();
 
     //duplicacion de los logos
-    /* const logos = document.getElementById("slider").cloneNode(true);
-    document.getElementById("logos").appendChild(logos); */
-
-
-    //despliegue de los item
-    const items = document.querySelectorAll('.list__item');
-
-    items.forEach(item => {
-        const icon = item.querySelector('.despliegue__icon i');
-
-        item.addEventListener('click', () => {
-            // Cierra los demás ítems
-            items.forEach(otherItem => {
-                if (otherItem !== item) {
-                    otherItem.classList.remove('active');
-                    const otherIcon = otherItem.querySelector('.despliegue__icon i');
-                    if (otherIcon) {
-                        otherIcon.classList.remove('ri-subtract-line');
-                        otherIcon.classList.add('ri-add-line');
-                    }
-                }
-            });
-
-            // Alterna el ítem actual
-            item.classList.toggle('active');
-
-            // Cambia el ícono
-            if (item.classList.contains('active')) {
-                icon.classList.remove('ri-add-line');
-                icon.classList.add('ri-subtract-line');
-            } else {
-                icon.classList.remove('ri-subtract-line');
-                icon.classList.add('ri-add-line');
-            }
-        });
-    });
-
-
-    //Overflow de los productos
-    const scrollContainer = document.querySelector('.deal__flex');
-
-    scrollContainer.addEventListener('mouseenter', () => {
-        scrollContainer.classList.add('scroll-active');
-    });
-
-    scrollContainer.addEventListener('mouseleave', () => {
-        scrollContainer.classList.remove('scroll-active');
-    });
-
-
-
+    const logos = document.getElementById("slider").cloneNode(true);
+    document.getElementById("logos").appendChild(logos);
 
 });
