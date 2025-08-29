5b7005c00d996808032c77f58e3d52cda3d50a71
J0SE <joseandrestorres0508@gmail.com>
2025-08-29 15:34:45 -0500

    feat: agregar controlador Pack.

---
+++ src/main/java/com/application/presentation/controller/pack/PackController.java
@@ -0,0 +1,78 @@
+package com.application.presentation.controller.pack;
+
+import com.application.persistence.entity.usuario.Usuario;
+import com.application.presentation.dto.general.response.GeneralResponse;
+import com.application.presentation.dto.pack.request.PackCreateRequest;
+import com.application.presentation.dto.pack.response.PackResponse;
+import com.application.service.implementation.pack.PackServiceImpl;
+import com.application.service.implementation.usuario.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
+
+import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/pack")
+public class PackController {
+
+    @Autowired
+    private PackServiceImpl packService;
+
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
+    @GetMapping("/")
+    public String Pack(@AuthenticationPrincipal UserDetails userDetails,
+                       @RequestParam(value = "mensaje", required = false) String mensaje,
+                       Model model) {
+
+        Usuario usuario = usuarioService.getUsuarioByCorreo(userDetails.getUsername());
+        List<PackResponse> packList = packService.getPacks();
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("packList", packList);
+        model.addAttribute("mensaje", mensaje);
+
+        return "Pack";
+    }
+
+    @PostMapping("/add-pack")
+    public String addPack(@ModelAttribute @Valid PackCreateRequest packRequest) {
+        GeneralResponse response = packService.addPack(packRequest);
+        String mensaje = response.mensaje();
+
+        return "redirect:/admin/pack/?mensaje=" + UriUtils.encode(mensaje, StandardCharsets.UTF_8);
+    }
+
+    @PostMapping("/update-pack/{id}")
+    public String updatePack(@ModelAttribute @Valid PackCreateRequest packRequest, @PathVariable Long id) {
+        GeneralResponse response = packService.updatePack(packRequest, id);
+        String mensaje = response.mensaje();
+
+        return "redirect:/admin/pack/?mensaje=" + UriUtils.encode(mensaje, StandardCharsets.UTF_8);
+    }
+
+    @PostMapping("/disable-pack/{id}")
+    public String updatePack(@PathVariable Long id) {
+        GeneralResponse response = packService.disablePack(id);
+        String mensaje = response.mensaje();
+
+        return "redirect:/admin/pack/?mensaje=" + UriUtils.encode(mensaje, StandardCharsets.UTF_8);
+    }
+
+    @PostMapping("/delete-pack/{id}")
+    public String deletePack(@PathVariable Long id) {
+        GeneralResponse response = packService.deletePack(id);
+        String mensaje = response.mensaje();
+
+        return "redirect:/admin/pack/?mensaje=" + UriUtils.encode(mensaje, StandardCharsets.UTF_8);
+    }
+
+}
