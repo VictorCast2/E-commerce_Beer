ef3d487ddf8aec44957b53623a4784a1474a25f5
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-09-17 17:13:26 -0500

    Add formatting improvements to Empresa entity
    Added blank lines between fields in the Empresa entity class for improved readability and consistency.

--- src/main/java/com/application/persistence/entity/empresa/Empresa.java
+++ src/main/java/com/application/persistence/entity/empresa/Empresa.java
@@ -31,22 +31,29 @@ public class Empresa {
     @Column(name = "nit")
     @Pattern(regexp = "", message = "El nit de la empresa debe ser válido")
     private String nit;
+
     @Column(name = "razon_social")
     private String razonSocial;
+
     @Column(name = "ciudad")
     private String ciudad;
+
     @Column(name = "direccion")
     private String direccion;
+
     @Column(name = "telefono")
     @Pattern(regexp = "^(\\+\\d{1,3}[- ])?\\d{10}$", message = "El número de teléfono debe tener 10 dígitos y puede incluir un código de país opcional.")
     private String telefono;
+
     @Column(name = "correo", length = 100)
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "El correo debe ser válido")
     @Email(message = "El correo debe ser válido")
     private String correo;
+
     @Column(name = "sector")
     @Enumerated(EnumType.STRING)
     private ESector eSector;
+
     @Column(name = "activo")
     private boolean activo;
 
